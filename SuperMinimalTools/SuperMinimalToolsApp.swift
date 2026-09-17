b4aac807cb21ed8d2ca443185d0a3054ca034a89
Tai Phan Van <taipv.swe@gmail.com>
2026-09-17 13:53:16 +0700

    Initial Commit

---
+++ SuperMinimalTools/SuperMinimalToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperMinimalToolsApp.swift
+//  SuperMinimalTools
+//
+//  Created by Tai Phan Van on 17/9/26.
+//
+
+import SwiftUI
+
+@main
+struct SuperMinimalToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SuperMinimalToolsTests/SuperMinimalToolsTests.swift
@@ -0,0 +1,19 @@
+//
+//  SuperMinimalToolsTests.swift
+//  SuperMinimalToolsTests
+//
+//  Created by Tai Phan Van on 17/9/26.
+//
+
+import Testing
+@testable import SuperMinimalTools
+
+struct SuperMinimalToolsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
